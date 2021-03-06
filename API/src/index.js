ee1f2fbf5bd5913a43e4dea73f47994078bca150
viniciussslima999 <viniciussslima@ufrn.edu.br>
2021-03-06 19:31:57 -0300

    Ajuste para poder receber o json no body
    Co-authored-by: Daniele Carvalho <daniele-mc@users.noreply.github.com>

--- API/src/index.js
+++ API/src/index.js
@@ -5,8 +5,8 @@ const express = require("express");
 const routes = require("./routes");
 
 const app = express();
-app.use(routes);
 app.use(express.json());
+app.use(routes);
 
 app.listen(3001, () => {
   console.log("API rodando na porta 3001");
