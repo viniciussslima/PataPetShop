f20de2ce06c4d8c627ae1ddbc11831606b0fd285
viniciussslima999 <viniciussslima@ufrn.edu.br>
2021-03-11 17:01:12 -0300

    Criação da rota /user

--- API/src/routes/index.js
+++ API/src/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const auth = require("./auth");
+const user = require("./user");
 
 router.use("/auth", auth);
+router.use("/user", user);
 
 module.exports = router;
