ade9f829689b49b5a17fb75e296a174100a68c79
viniciussslima999 <viniciussslima@ufrn.edu.br>
2021-03-11 17:00:32 -0300

    Implementação do logout
    Co-authored-by: Daniele Carvalho <daniele-mc@users.noreply.github.com>

--- API/src/routes/auth/index.js
+++ API/src/routes/auth/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const createController = require("./controller/createController");
 const loginController = require("./controller/loginController");
+const logoutController = require("./controller/logoutController");
 
 router.post("/create", createController);
 router.post("/login", loginController);
+router.post("/logout", logoutController);
 
 module.exports = router;
