9142cf080607cd5e55e9f1e769f46c9eab19ad1f
viniciussslima999 <viniciussslima@ufrn.edu.br>
2021-03-13 20:44:00 -0300

    Implementação de testes para as rotas já existentes
    Co-authored-by: Daniele Carvalho <daniele-mc@users.noreply.github.com>

---
+++ API/tests/create.test.js
@@ -0,0 +1,23 @@
+const server = require("../src/index");
+const request = require("supertest");
+
+describe("Endpoint auth/create", () => {
+  it("Success", async () => {
+    const response = await request(server)
+      .post("/auth/create")
+      .send({ username: "teste", password: "1234" });
+
+    expect(response.status).toBe(200);
+  });
+});
+
+describe("Endpoint auth/create", () => {
+  it("Fail", async () => {
+    const response = await request(server)
+      .post("/auth/create")
+      .send({ username: "teste", password: "1234" });
+
+    expect(response.status).toBe(409);
+    expect(response.body).toEqual({ message: "Esse usuário já existe" });
+  });
+});
---
+++ API/tests/getUser.test.js
@@ -0,0 +1,110 @@
+const server = require("../src/index");
+const request = require("supertest");
+const login = require("./login");
+
+describe("Endpoint /user", () => {
+  it("Success", async () => {
+    const token = await login("teste1");
+    const response = await request(server)
+      .get("/user")
+      .set("x-access-token", token)
+      .send();
+
+    let expectArray = [
+      { username: "teste1", type: "admin" },
+      { username: "teste3", type: "vet" },
+      { username: "teste4", type: "seller" },
+      { username: "teste5", type: "washer" },
+    ];
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(expectArray);
+  });
+});
+
+describe("Endpoint /user", () => {
+  it("Success", async () => {
+    const token = await login("teste1");
+    const response = await request(server)
+      .get("/user")
+      .set("x-access-token", token)
+      .query({ type: "admin" })
+      .send();
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual([{ username: "teste1", type: "admin" }]);
+  });
+});
+
+describe("Endpoint /user", () => {
+  it("Success", async () => {
+    const token = await login("teste1");
+    const response = await request(server)
+      .get("/user")
+      .set("x-access-token", token)
+      .query({ type: "vet" })
+      .send();
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual([{ username: "teste3", type: "vet" }]);
+  });
+});
+
+describe("Endpoint /user", () => {
+  it("Success", async () => {
+    const token = await login("teste1");
+    const response = await request(server)
+      .get("/user")
+      .set("x-access-token", token)
+      .query({ type: "seller" })
+      .send();
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual([{ username: "teste4", type: "seller" }]);
+  });
+});
+
+describe("Endpoint /user", () => {
+  it("Success", async () => {
+    const token = await login("teste1");
+    const response = await request(server)
+      .get("/user")
+      .set("x-access-token", token)
+      .query({ type: "washer" })
+      .send();
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual([{ username: "teste5", type: "washer" }]);
+  });
+});
+
+describe("Endpoint /user", () => {
+  it("Fail", async () => {
+    const token = await login("teste2");
+    const response = await request(server)
+      .get("/user")
+      .set("x-access-token", token)
+      .send();
+
+    expect(response.status).toBe(401);
+    expect(response.body).toEqual({
+      message: "Você não tem permissão para isso",
+    });
+  });
+});
+
+describe("Endpoint /user", () => {
+  it("Fail", async () => {
+    const token = await login("teste1");
+    const response = await request(server)
+      .get("/user")
+      .set("x-access-token", token)
+      .query({ type: "client" })
+      .send();
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({
+      message: "O valor do campo type é inválido",
+    });
+  });
+});
