5b95789d7932d0c3f91ec321370dfa62a12f32b9
viniciussslima999 <vinicius.silva.017@ufrn.edu.br>
2021-04-03 17:30:13 -0300

    Criação dos testes da rota POST de product
    Co-authored-by: Daniele Carvalho <daniele-mc@users.noreply.github.com>

---
+++ API/tests/product/createProduct.test.js
@@ -0,0 +1,60 @@
+const server = require("../../src/index");
+const request = require("supertest");
+const { login } = require("../helpers");
+
+describe("Endpoint product", () => {
+  it("Success", async () => {
+    const token = await login("teste1", "1234");
+    const response = await request(server)
+      .post("/product/")
+      .set("x-access-token", token)
+      .send({
+        name: "Ração de cachorro",
+        description: "Ração sabor frango",
+        price: 130,
+        stock: 10,
+      });
+
+    expect(response.status).toBe(200);
+  });
+});
+
+describe("Endpoint product", () => {
+  it("Fail", async () => {
+    const token = await login("teste1", "1234");
+    const response = await request(server)
+      .post("/product")
+      .set("x-access-token", token)
+      .send({
+        name: "Ração de cachorro",
+        description: "Ração sabor frango",
+        price: 55,
+        stock: 10,
+      });
+
+    expect(response.status).toBe(409);
+    expect(response.body).toEqual({
+      message: "Esse produto já existe",
+    });
+  });
+});
+
+describe("Endpoint product", () => {
+  it("Fail", async () => {
+    const token = await login("teste2", "1234");
+    const response = await request(server)
+      .post("/product")
+      .set("x-access-token", token)
+      .send({
+        name: "Ração de cachorro",
+        description: "Ração sabor frango",
+        price: 55,
+        stock: 10,
+      });
+
+    expect(response.status).toBe(401);
+    expect(response.body).toEqual({
+      message: "Você não tem permissão para isso",
+    });
+  });
+});
