f8271661cb85de5ee2025427346f4332e220a664
viniciussslima999 <vinicius.silva.017@ufrn.edu.br>
2021-04-11 19:31:19 -0300

    Criação dos testes da rota /cart

---
+++ tests/cart/buy2.test.js
@@ -0,0 +1,18 @@
+const server = require("../../src/index");
+const request = require("supertest");
+const { login } = require("../helpers");
+
+describe("Endpoint cart", () => {
+  it("Fail", async () => {
+    const token = await login("teste1", "1234");
+    const response = await request(server)
+      .post("/cart/buy")
+      .set("x-access-token", token)
+      .send();
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({
+      message: "O produto 'Ração de cachorro' não está disponível no estoque",
+    });
+  });
+});
---
+++ tests/mysql/cart/addProduct.sql
@@ -0,0 +1,37 @@
+CREATE SCHEMA IF NOT EXISTS `PataPetShop` ;
+USE `PataPetShop` ;
+
+CREATE TABLE IF NOT EXISTS `PataPetShop`.`user` (
+  `username` VARCHAR(255) NOT NULL,
+  `password` CHAR(64) NOT NULL,
+  `type` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`username`))
+DEFAULT CHARACTER SET = utf8;
+  
+CREATE TABLE IF NOT EXISTS `PataPetShop`.`product` (
+  `name` VARCHAR(255) NOT NULL,
+  `description` VARCHAR(255) NULL DEFAULT NULL,
+  `price` FLOAT NOT NULL,
+  `stock` INT NOT NULL,
+  PRIMARY KEY (`name`))
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `PataPetShop`.`cart_product` (
+  `qty` INT NOT NULL,
+  `product` VARCHAR(255) NOT NULL,
+  `user` VARCHAR(255) NOT NULL,
+  INDEX `fk_cart_product_product1_idx` (`product` ASC) VISIBLE,
+  INDEX `fk_cart_product_user1_idx` (`user` ASC) VISIBLE,
+  PRIMARY KEY (`user`, `product`),
+  CONSTRAINT `fk_cart_product_product1`
+    FOREIGN KEY (`product`)
+    REFERENCES `PataPetShop`.`product` (`name`),
+  CONSTRAINT `fk_cart_product_user1`
+    FOREIGN KEY (`user`)
+    REFERENCES `PataPetShop`.`user` (`username`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+DEFAULT CHARACTER SET = utf8;
+
+INSERT INTO `user` VALUES ('teste1', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'admin');
+INSERT INTO `product` VALUES ('produto1', 'descricao1', 10.1, 10);
